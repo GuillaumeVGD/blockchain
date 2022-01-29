77c122485cc7b6be905ccabf9674e7048449abe6
GuillaumeVGD <guillaume.f.services@gmail.com>
2022-01-29 13:27:30 +0100

    rewriting bassis

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from objects import *
 
 block=1
-nonce=0
+pof=0
 message="message"
 previous="0000000000000000000000000000000000000000000000000000000000000000"
 
-Block1=Block(block,nonce,message,previous)
-Block1.signature()
-print(Block1.hash)
+Block1=Block(block,pof,message,previous)
+Block1.show()
 
-#def signature(block,nonce,message,previous):
-#    data=(message+str(block)+str(nonce))
-#    hash= hashlib.sha256(data.encode()).hexdigest()
-#    while(str(hash[:4])!="0000"):
-#        nonce+=1
-#        data=(str(block)+str(nonce)+message+previous)
-#        hash=hashlib.sha256(data.encode()).hexdigest()
-#        #print(nonce,"   ",hash)
-#    return block,nonce,hash,previous
-#
-#print("final nonce = ",signature(block,nonce,message,previous)[1])
-#print("final hash = ",signature(block,nonce,message,previous)[2])
+
 
--- objects.py
+++ objects.py
@@ -6,13 +6,17 @@ class Block:
         self.nonce=nonce
         self.message=message
         self.previous=previous
-
-    def signature(self):
         self.data=(self.message+str(self.block)+str(self.nonce))
         self.hash= hashlib.sha256(self.data.encode()).hexdigest()
         while(str(self.hash[:4])!="0000"):
             self.nonce+=1
             self.data=(str(self.block)+str(self.nonce)+self.message+self.previous)
             self.hash=hashlib.sha256(self.data.encode()).hexdigest()
-            #print(nonce,"   ",hash)
-        return self.block,self.nonce,self.hash,self.previous
+    
+    def show(self):
+        print("BLOCK = ",self.block," ; Hash= ",self.hash," ; POF = ", self.nonce," ;")
+
+#class NewBlock:
+#    def __init__(self,PrevBlock,message):
+#        self.PrevBlock=PrevBlock
+#        self.message=message

6238e862c16067fefdb58e0fe1b9322a42935f9c
GuillaumeVGD <guillaume.f.services@gmail.com>
2022-01-30 18:58:26 +0100

    first chain system

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from objects import *
 
-id=1
-prev= "0000000000000000000000000000000000000000000000000000000000000000"
-content= "on va tester autre chose"
+#id=1
+#prev= "0000000000000000000000000000000000000000000000000000000000000000"
+content= "la blockchain marche !"
 sign= "guillaume"
 
+def read():
+    with open('chain.txt','r') as rf:
+        lastBlock=rf.readlines()[-1]
+        sep=lastBlock.split(",")
+    return sep
 
-Block1=Block(id,prev,content,sign)
-Block1.show()
+Block1=Block(read()[0],read()[1],content,sign)
+
+
+
 
 
 
--- objects.py
+++ objects.py
@@ -2,7 +2,7 @@ import hashlib
 
 class Block:
     def __init__(self,id,prev,content,sign):
-        self.id=id
+        self.id=int(id)+1
         self.prev=prev
         self.content=content
         self.sign=sign
@@ -15,8 +15,16 @@ class Block:
             self.pof+=1
             self.data=(str(self.id)+str(self.prev)+str(self.content)+str(self.sign)+str(self.pof))
             self.hash=hashlib.sha256(self.data.encode()).hexdigest()
+        
+        line=str(self.id)+","+str(self.prev)+","+str(self.content)+","+str(self.sign)+","+str(self.pof)+","+str(self.hash) 
+        with open('chain.txt','a') as af:
+            af.write(line+"\n")
     
     def show(self):
         print("ID = ",self.id," ; PREV = ",self.prev," ; CONTENT = ", self.content," ; SIGN = ",self.sign," ; POF = ",self.pof," ; HASH = ",self.hash," ;")
 
+    #def print(self):
+        
 
+
+    
